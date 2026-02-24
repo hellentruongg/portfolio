2569cb0c392a7a036d754a3d9951fdf7bbe92d2c
hellen truong <hellentruong123@gmail.com>
2026-02-24 09:44:59 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const sidemenu = document.querySelector("#sidemenu");
+const navbar = document.querySelector("nav");
+const navmenu = document.querySelector("nav ul");
+const navlinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll(".section");
+
+function openMenu() {
+  sidemenu.style.transform = "translateX(-16rem)";
+}
+
+function closeMenu() {
+  sidemenu.style.transform = "translateX(16rem)";
+}
+
+window.addEventListener("scroll", () => {
+  if (scrollY > 50) {
+    navbar.classList.add("bg-white/50", "backdrop-blur-lg", "shadow-sm");
+    navmenu.classList.remove("bg-white/50", "shadow-sm");
+  } else {
+    navbar.classList.remove("bg-white/50", "backdrop-blur-lg", "shadow-sm");
+    navmenu.classList.add("bg-white/50", "shadow-sm");
+  }
+});
+
+const isMobile = window.innerWidth < 768;
+
+const threshold = isMobile ? 0.5 : 0.9;
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+
+    entries.forEach((entry) => {
+      const id = entry.target.id;
+      const correspondingLinks = document.querySelectorAll(
+        `.nav-link[href="#${id}"]`,
+      );
+
+      if (entry.isIntersecting) {
+        navlinks.forEach((link) =>
+          link.classList.remove(
+            "font-bold",
+            "bg-gradient-to-r",
+            "from-purple-300",
+            "to-pink-300",
+            "bg-clip-text",
+            "text-transparent",
+          ),
+        );
+        correspondingLinks.forEach((link) =>
+          link.classList.add(
+            "font-bold",
+            "bg-gradient-to-r",
+            "from-purple-300",
+            "to-pink-300",
+            "bg-clip-text",
+            "text-transparent",
+          ),
+        );
+      }
+    });
+  },
+  { threshold: threshold },
+);
+
+sections.forEach((section) => observer.observe(section));
